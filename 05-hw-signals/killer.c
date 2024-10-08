9d0e1533e85c70a82fd1d158bd5bb3b5cc59fda4
Weslee Bovee <wbovee@arizona.cs.byu.edu>
2024-10-07 20:40:46 -0600

    all but 6 and 8

--- 05-hw-signals/killer.c
+++ 05-hw-signals/killer.c
@@ -2,52 +2,108 @@
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
-
 #include <sys/types.h>
 #include <sys/wait.h>
 
 void sigint_handler(int signum) {
-	// send SIGKILL to all processes in group, so this process and children
-	// will terminate.  Use SIGKILL because SIGTERM and SIGINT (among
-	// others) are overridden in the child.
-	kill(-getpgid(0), SIGKILL);
+    kill(-getpgid(0), SIGKILL);
 }
-
 int main(int argc, char *argv[]) {
-	char *scenario = argv[1];
-	int pid = atoi(argv[2]);
+    char *scenario = argv[1];
+    int pid = atoi(argv[2]);
+    struct sigaction sigact;
 
-	struct sigaction sigact;
+    sigact.sa_flags = SA_RESTART;
+    sigact.sa_handler = sigint_handler;
+    sigaction(SIGINT, &sigact, NULL);
 
-	// Explicitly set flags
-	sigact.sa_flags = SA_RESTART;
-	sigact.sa_handler = sigint_handler;
-	// Override SIGINT, so that interrupting this process sends SIGKILL to
-	// this one and, more importantly, to the child.
-	sigaction(SIGINT, &sigact, NULL);
-
-	switch (scenario[0]) {
-	case '0':
-		break;
+    switch (scenario[0]) {
+        case '0':
+            kill(pid, SIGHUP);
+            sleep(1);
+            break;
 	case '1':
-		break;
-	case '2':
-		break;
-	case '3':
-		break;
-	case '4':
-		break;
-	case '5':
-		break;
-	case '6':
-		break;
-	case '7':
-		break;
-	case '8':
-		break;
-	case '9': 
-		break;
-
-	}
-	waitpid(pid, NULL, 0);
+	    kill(pid, 12);
+            sleep(1);
+	    kill(pid,SIGTERM);
+	    sleep(1);
+	    break;
+        case '2':
+            kill(pid, SIGHUP);
+            sleep(1);
+	    kill(pid,12);
+            kill(pid, SIGTERM);
+            sleep(1);
+            break;
+        case '3':
+            kill(pid, SIGHUP);
+	    kill(pid, SIGINT);
+            sleep(5);
+            kill(pid, 12);
+            sleep(1);
+            kill(pid, SIGTERM);
+            sleep(1);
+            break;
+        case '4':
+            kill(pid, SIGHUP);
+            sleep(1);
+            kill(pid, SIGINT);
+            sleep(1);
+	    kill(pid, 12);
+	    sleep(1);
+	    kill(pid, SIGTERM);
+	    sleep(1);
+            break;
+        case '5':
+            kill(pid, SIGHUP);
+	    kill(pid, 12);
+            sleep(1);
+	    kill(pid, SIGTERM);
+	    sleep(1);
+            break;
+        case '6':
+            kill(pid, SIGHUP);
+            sleep(1);
+            kill(pid, 10);
+            sleep(2);
+	    kill(pid, 12);
+            sleep(1);
+	    kill(pid,SIGTERM);
+            sleep(1);
+            break;
+        case '7':
+            kill(pid, SIGHUP);
+            sleep(1);
+            kill(pid, 10);
+	    sleep(1);
+	    kill(pid, 12);
+	    sleep(1);
+	    kill(pid, SIGTERM);
+	    sleep(1);
+            break;
+        case '8':
+            kill(pid, SIGHUP);
+            sleep(1);
+	    kill(pid, 30);
+	    sleep(1);
+	    kill(pid,SIGTERM);
+	    sleep(1);
+	    kill(pid, 12);
+	    sleep(1);
+	    kill(pid, SIGTERM);
+	    sleep(1);
+            break;
+        case '9':
+            kill(pid, 31);
+            sleep(1);
+	    kill(pid, SIGQUIT);
+	    kill(pid, 31);
+	    sleep(5);
+	    kill(pid, 12);
+	    sleep(1);
+	    kill(pid, SIGTERM);
+	    sleep(1);
+            break;
+    }
+    waitpid(pid, NULL, 0);
 }
